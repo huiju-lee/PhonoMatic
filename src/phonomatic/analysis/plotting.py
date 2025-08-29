7f4ea92159163fd7198c1d982bcfa1f71d1db512
Rylan Harrison <rylan.harr88@gmail.com>
2025-08-29 18:41:43 -0400

    Enable users to add DOS augmentations when creating PDF of phonon dispersion plots for all materials in a directory

--- src/phonomatic/analysis/plotting.py
+++ src/phonomatic/analysis/plotting.py
@@ -25,30 +25,56 @@ def _validate_dos_augment(dos_augment):
 
 def _setup_dispersion_fig(
     dos_augment=None, 
-    figure_kwargs=None
+    figure_kwargs=None, 
+    num_plots=1
 ):
     default_figure_kwargs = {"figsize": (6, 4)}
     figure_kwargs = {**default_figure_kwargs, **(figure_kwargs or {})}
-    
-    if dos_augment is not None:
-        if (dos_augment == 'pdos' or dos_augment == 'total_dos'):
-            cols = 2  # 1 row, 2 columns (panels)
-            width_ratios =  [2, 1]
-        elif dos_augment == 'both':
-            cols = 3
-            width_ratios = [2, 1, 1]
-            
-        fig, axes = plt.subplots(
-            nrows=1,
-            ncols=cols,
-            sharey=(cols > 1),  # Share axis if we have multiple panels
-            gridspec_kw = {"width_ratios": width_ratios},
-            **figure_kwargs
-        )
+    rows, cols = _get_page_layout(num_plots)
+
+    if (dos_augment == 'pdos' or dos_augment == 'total_dos'):
+        panels_per_plot = 2  
+        width_ratios =  [2, 1]
+    elif dos_augment == 'both':
+        panels_per_plot = 3
+        width_ratios = [2, 1, 1]
     else:
-        fig, ax = plt.subplots(**figure_kwargs)
-        axes = [ax] # Make iterable for consistency
-    return fig, axes
+        panels_per_plot = 1
+        width_ratios = [1]
+        
+    fig = plt.figure(**figure_kwargs)
+    all_axes = []
+
+    # One grid for entire page or image
+    outer_gs = fig.add_gridspec(
+        nrows=rows,
+        ncols=cols,
+    )
+
+    for idx in range(num_plots):
+        # Get row and col corresponding to that index
+        r, c = divmod(idx, cols)
+        # Sub-gridspec for this plot group
+        sub_gs = outer_gs[r, c].subgridspec(
+            nrows=1,
+            ncols=panels_per_plot,
+            width_ratios=width_ratios
+        )
+
+        group_axes = []
+        # For each panel, add a subplot
+        for j in range(panels_per_plot):
+            if j == 0:
+                ax = fig.add_subplot(sub_gs[j])
+            else:
+                # Extra plots after first (the phonon dispersion plot)
+                # should share y-axis with phonon dispersion plot
+                ax = fig.add_subplot(sub_gs[j], sharey=group_axes[0])
+            group_axes.append(ax)
+
+        all_axes.append(group_axes)
+
+    return fig, all_axes
 
 
 def _get_dos_paths(band_yaml_paths, dos_augment):
@@ -84,7 +110,7 @@ def plot_phonon_dispersion(
         legend_kwargs=None, 
         postprocess=None, 
         dos_augment=None,
-        ax=None    
+        axes=None    
 ):
     """
     Plot phonon dispersion curves from a list of phonopy band.yaml files.
@@ -113,6 +139,10 @@ def plot_phonon_dispersion(
     if line_styles is None:
         line_styles = [{'color': 'C' + str(i)} 
                        for i in range(len(yaml_files))]
+    # If single dict was passed, convert to list for cycling
+    elif isinstance(line_styles, dict):
+        line_styles = [line_styles]
+
     # Cycle through line styles if there are fewer styles than datasets
     style_cycler = cycle(line_styles)
 
@@ -124,11 +154,16 @@ def plot_phonon_dispersion(
 
     # Decide whether to create our own figure - we do if none was passed
     own_fig = False
-    if ax is None:
+    if axes is None:
         own_fig = True
         output_path = create_output_file(output_path, "band_plot.png")
         _validate_dos_augment(dos_augment)
+        # In the case where we create a PDF, _setup_dispersion_fig is called
+        # in _make_dispersion_page
         fig, axes = _setup_dispersion_fig(dos_augment, figure_kwargs)
+        # _setup_dispersion_fig returns a list of lists - unpack to get
+        # list of axes for this singular plot
+        axes = axes[0]  
 
     axis_kwargs = axis_kwargs or {}
     tick_params = axis_kwargs.get(
@@ -150,6 +185,7 @@ def plot_phonon_dispersion(
     for (dist, freqs, _, _, _), label in zip(band_data, labels):
         style = next(style_cycler)
         for j in range(freqs.shape[0]):
+            print("style type:", type(style), style)
             dispersion_ax.plot(dist, freqs[j],
                                label=label if j == 0 else "", **style)
 
@@ -175,6 +211,7 @@ def plot_phonon_dispersion(
         
     # Let the user make additional stylistic changes beyond the parameters
     # included in this function
+    # TO-DO: Allow post-processing of DOS augmentations
     if postprocess:
         postprocess(dispersion_ax)
 
@@ -252,7 +289,8 @@ def _make_dispersion_page(
     axis_kwargs,
     figure_kwargs,
     postprocess_subplot, 
-    postprocess_page
+    postprocess_page, 
+    dos_augment
 ):
     """
     Creates a single page of phonon dispersion plots.
@@ -279,15 +317,10 @@ def _make_dispersion_page(
         fig (matplotlib.figure.Figure): The page of phonon dispersion curves.
     """
 
-    # Define layout for this page
-    n_row, n_col = _get_page_layout(len(batch))
-    default_fig_kwargs = {"figsize": (8.5, 11), "constrained_layout": True}
-    # Merge defaults with user-provided
-    figure_kwargs = {**default_fig_kwargs, **(figure_kwargs or {})}
-    fig, axs = plt.subplots(n_row, n_col, **figure_kwargs)
-    
-    # Flatten to 1D array for easier indexing
-    axs = np.atleast_1d(axs).flatten()
+    # Define layout of page
+    fig_kwargs = {"figsize": (8.5, 11), "constrained_layout": True}
+    fig, group_axs = _setup_dispersion_fig(dos_augment, fig_kwargs, 
+                                     num_plots=len(batch))
 
     # Cycle through user-supplied plotting and stylistic 
     # Next set of line styles for a given submplot
@@ -296,22 +329,24 @@ def _make_dispersion_page(
     postprocess_next = _make_cycler(postprocess_subplot)
 
     # Plot each phonon dispersion curve in the batch
-    for ax, yaml_group in zip(axs, batch):
+    for group_ax, yaml_group in zip(group_axs, batch):
         plot_phonon_dispersion(
             yaml_group,
-            ax=ax,
+            axes=group_ax,
             line_styles=line_styles_next(),
             axis_kwargs=axis_kwargs_next(),
             postprocess=postprocess_next(),
+            dos_augment=dos_augment
         )
 
     # Turn off unused subplots
-    for ax in axs[len(batch):]:
-        ax.axis('off')
+    for group_ax in group_axs[len(batch):]:
+        for ax in group_ax:
+            ax.axis('off')
 
     # Add legend
     if common_legend:
-        handles, labels = axs[0].get_legend_handles_labels()
+        handles, labels = group_axs[0][0].get_legend_handles_labels()
         default_args = dict(loc="lower center", bbox_to_anchor=(0.5, -0.05), 
                             fontsize=10, ncol=1)
         # Merge user-provided legend parameters with the defaults
@@ -321,7 +356,7 @@ def _make_dispersion_page(
     # Allow user to pass additional function to modify page if they
     # weren't able to do so with the hard-coded parameters
     if postprocess_page:
-        postprocess_page(fig, axs)
+        postprocess_page(fig, group_axs)
 
     return fig
             
@@ -336,7 +371,8 @@ def plot_all_dispersion_curves(
         axis_kwargs=None, 
         figure_kwargs=None,
         postprocess_subplot=None, 
-        postprocess_page=None
+        postprocess_page=None, 
+        dos_augment=None
 ):
     """
     Plots phonon dispersion curves for all materials contained in a
@@ -404,7 +440,8 @@ def plot_all_dispersion_curves(
                               axis_kwargs=axis_kwargs,
                               figure_kwargs=figure_kwargs,
                               postprocess_subplot=postprocess_subplot, 
-                              postprocess_page=postprocess_page)
+                              postprocess_page=postprocess_page, 
+                              dos_augment=dos_augment)
         for batch in batched_paths
     ]
 
